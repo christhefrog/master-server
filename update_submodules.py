80716eef987116b829db58fb9e241cba99fec34f
chris <47784402+christhefrog@users.noreply.github.com>
2025-02-17 12:03:49 +0100

    Removed unnecessary commands

--- update_submodules.py
+++ update_submodules.py
@@ -1,16 +1,12 @@
 import subprocess
 
-submodule_update_command = "git submodule update --remote --merge"
-submodule_pull_command = "git submodule foreach --recursive git pull"
+subprocess.run("git submodule update --remote --merge", shell=True, check=True)
 
-subprocess.run(submodule_update_command, shell=True, check=True)
-subprocess.run(submodule_pull_command, shell=True, check=True)
-
-status_result = subprocess.run("git status --porcelain", shell=True, check=True, stdout=subprocess.PIPE)
+status_result = subprocess.run("git status ./routes --porcelain", shell=True, check=True, stdout=subprocess.PIPE)
 
 if status_result.stdout:
     subprocess.run("git add ./routes", shell=True, check=True)
-    subprocess.run("git commit -m \"Update submodules\"", shell=True, check=True)
+    subprocess.run("git commit -m \"Updated submodules\"", shell=True, check=True)
     #subprocess.run("git push", shell=True, check=True)
     print("Submodules update commited! ✅")
 else:
