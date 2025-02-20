74572a9995bfc20b38cc50b5a0fecbead0e90af7
chris <47784402+christhefrog@users.noreply.github.com>
2025-02-20 16:23:21 +0100

    Updated submodule script

--- update_submodules.py
+++ update_submodules.py
@@ -5,7 +5,7 @@ subprocess.run("git submodule update --remote --merge", shell=True, check=True)
 status_result = subprocess.run("git status ./routes --porcelain", shell=True, check=True, stdout=subprocess.PIPE)
 
 if status_result.stdout:
-    subprocess.run("git add ./routes", shell=True, check=True)
+    subprocess.run("git add ./routes .gitmodules", shell=True, check=True)
     subprocess.run("git commit -m \"Updated submodules\"", shell=True, check=True)
     #subprocess.run("git push", shell=True, check=True)
     print("Submodules update commited! ✅")
